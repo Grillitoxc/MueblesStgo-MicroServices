c71a052bd89194e766ee5db15f005fd9763043d7
Christopher Torres Aceituno <christopher.torres@usach.cl>
2022-10-26 00:28:11 -0300

    [testing]: Testing the gateway connection

--- gateway-service/src/main/java/Tingeso2Microservices/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/Tingeso2Microservices/gatewayservice/GatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package Tingeso2Microservices.employeeservice;
+package Tingeso2Microservices.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EmployeeServiceApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
